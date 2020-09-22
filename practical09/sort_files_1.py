24dbf970347e65cb4aa054a2bd087c9dc95b2388
ace <631479724@qq.com>
2020-09-22 11:19:05 +0800

    practical 05

--- practical09/sort_files_1.py
+++ practical09/sort_files_1.py
@@ -1,57 +1,31 @@
 import os
 import shutil
 
+PATH = "FilesToSort"
+
 
 def main():
-    os.chdir("FilesToSort")
+    os.chdir(PATH)
     files = os.listdir('.')
     dir_list = []
     for file in files:
-        suffix = file.split('.')
+        suffix = get_name_list(file)
         dir_list.append(suffix[1])
-
-    try:
-        for dir in dir_list:
+    for dir in dir_list:
+        try:
             os.mkdir(dir)
-        os.mkdir('doc')
-        os.mkdir('docx')
-        os.mkdir('xls')
-        os.mkdir('xlsx')
-        os.mkdir('png')
-        os.mkdir('jpg')
-        os.mkdir('gif')
-        os.mkdir('txt')
-    except FileExistsError:
-        pass
-    filenames = os.listdir('.')
-    print(filenames)
-
-    for filename in filenames:
-        if os.path.isfile(filename):
-            list_for_name = get_name_list(filename)
-            if list_for_name[1] == 'doc':
-                shutil.move(filename, 'doc/' + filename)
-
-            if list_for_name[1] == 'docx':
-                shutil.move(filename, 'docx/' + filename)
-
-            if list_for_name[1] == 'xls':
-                shutil.move(filename, 'xls/' + filename)
-
-            if list_for_name[1] == 'xlsx':
-                shutil.move(filename, 'xlsx/' + filename)
-
-            if list_for_name[1] == 'jpg':
-                shutil.move(filename, 'jpg/' + filename)
-
-            if list_for_name[1] == 'png':
-                shutil.move(filename, 'png/' + filename)
-
-            if list_for_name[1] == 'gif':
-                shutil.move(filename, 'gif/' + filename)
-
-            if list_for_name[1] == 'txt':
-                shutil.move(filename, 'txt/' + filename)
+        except FileExistsError:
+            pass
+    for dir in dir_list:
+        os.chdir(dir)
+        target = os.getcwd()
+        os.chdir("..")
+        for file in files:
+            file_type = get_name_list(file)
+            if file_type[1] == dir:
+                print("moving {} to {}".format(file, dir))
+                shutil.move(file, target)
+                files.pop(files.index(file))
 
 
 def get_name_list(filename):
